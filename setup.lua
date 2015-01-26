e2a6eed07e863459ff75447e3987e4765b019b3a
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-01-26 12:43:16 +0900

    GNU Autotoolsのインストールスクリプトを追加。

--- setup.lua
+++ setup.lua
@@ -35,32 +35,94 @@ end
 
 local function eval(...)
   local handle = assert(io.popen(string.format(...)))
-  local result = handle:read "*a":gsub("\n+$", "")
+  local result = handle:read("*a"):gsub("\n+$", "")
   assert(handle:close())
   return result
+end
+
+local function gnu_version(command)
+  local result = eval("%s --version 2>/dev/null || :", quote(command))
+  if result:match("Free Software Foundation, Inc%.") then
+    return result:match "[^\n]*":match "[%.%d]+$"
+  else
+    return nil
+  end
+end
+
+local function fetch(url)
+  local filename = assert(url:match("/([^/]+)$"))
+  exec("curl -fLO %s", quote(url))
+  exec("gzip -cd %s | tar xf -", quote(filename))
+end
+
+local function build(name, script)
+  if script == nil then
+    script = "./configure --prefix=\"$dromozoa_prefix\" && make && make install"
+  end
+  exec("cd %s && %s", quote(name), script)
 end
 
 local major, minor = assert(_VERSION:match("^Lua (%d+)%.(%d+)$"))
 local version = tonumber(major) * 100 + tonumber(minor)
 assert(version >= 503)
 
-local prefix = assert(os.getenv "dromozoa_prefix")
-
 do
   local package_version = "2.2.0"
   local package_name = "luarocks-" .. package_version
-  local package_filename = package_name .. ".tar.gz"
-  local package_url = "file:/Users/moyu/Downloads/" .. package_filename
+  local package_url = "file:/Users/moyu/Downloads/" .. package_name .. ".tar.gz"
 
-  local current_version
-  pcall(function ()
-    current_version = eval([[%s/bin/luarocks --version 2>/dev/null]], quote(prefix)):match(" ([%.%d]+)")
-  end)
+  local current_version = eval("\"$dromozoa_prefix/bin/luarocks\" --version 2>/dev/null || :"):match(" ([%.%d]+)")
   if current_version ~= package_version then
-    exec([[curl -fLO %s]], quote(package_url))
-    exec([[gzip -cd %s | tar xf -]], quote(package_filename))
-    exec([[cd %s && ./configure --prefix=%s && make build && make install]], quote(package_name), quote(prefix))
+    fetch(package_url)
+    build(package_name, "./configure --prefix=\"$dromozoa_prefix\" && make build && make install")
   end
 end
 
+do
+  local current_version = eval("perl -v 2>/dev/null || :"):match("v([%.%d]+)")
+  print("perl", current_version)
+end
 
+do
+  local current_version = gnu_version "m4"
+  print("GNU m4", current_version)
+end
+
+do
+  local package_version = "2.69"
+  local package_name = "autoconf-" .. package_version
+  local package_url = "file:///Users/moyu/Downloads/" .. package_name .. ".tar.gz"
+
+  local current_version = gnu_version "autoconf"
+  print("GNU autoconf", current_version)
+  if current_version ~= package_version then
+    fetch(package_url)
+    build(package_name)
+  end
+end
+
+do
+  local package_version = "1.15"
+  local package_name = "automake-" .. package_version
+  local package_url = "file:///Users/moyu/Downloads/" .. package_name .. ".tar.gz"
+
+  local current_version = gnu_version "automake"
+  print("GNU automake", current_version)
+  if current_version ~= package_version then
+    fetch(package_url)
+    build(package_name)
+  end
+end
+
+do
+  local package_version = "2.4.5"
+  local package_name = "libtool-" .. package_version
+  local package_url = "file:///Users/moyu/Downloads/" .. package_name .. ".tar.gz"
+
+  local current_version = gnu_version "libtool"
+  print("GNU libtool", current_version)
+  if current_version ~= package_version then
+    fetch(package_url)
+    build(package_name)
+  end
+end
