c291526d44c17a5e7335788b74836e79a43ad728
Tomoyuki Fujimori <moyu@dromozoa.com>
2024-03-30 20:30:20 +0900

    libsodiumを追加。

--- run-home.sh
+++ run-home.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with dromozoa-boot. If not, see <https://www.gnu.org/licenses/>.
 
-./dromozoa-boot --prefix="$HOME/prefixes/dromozoa" m4 autoconf automake libtool screen tig zeromq libjpeg libpng hook
+./dromozoa-boot --prefix="$HOME/prefixes/dromozoa" m4 autoconf automake libtool screen tig libsodium zeromq libjpeg libpng hook
 ./dromozoa-boot --prefix="$HOME/prefixes/dromozoa51" lua51 luajit luarocks vim
 ./dromozoa-boot --prefix="$HOME/prefixes/dromozoa52" lua52 luarocks vim
 ./dromozoa-boot --prefix="$HOME/prefixes/dromozoa53" lua53 luarocks vim
